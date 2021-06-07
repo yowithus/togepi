c99775e76f75a3be1ec1674e39305236ec2d2648
yonatan-nugraha <yonatan.nugraha@hotmail.com>
2021-06-07 17:37:41 +0700

    init project

---
+++ src/main/java/com/example/togepi/TogepiApplication.java
@@ -0,0 +1,13 @@
+package com.example.togepi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogepiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogepiApplication.class, args);
+	}
+
+}
